79cd1a0e3369a0d16d9b5eecd57b561efd649160
Stefan Silviu <stefan.silviu.alexandru@gmail.com>
2022-05-10 20:44:21 +0300

    Create user service

---
+++ src/app/base/services/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private readonly currentUserSubject: BehaviorSubject<User | null> = new BehaviorSubject<User | null>(null);
+
+  public readonly currentUser$: Observable<User | null> = this.currentUserSubject;
+
+  constructor() {
+    // FIXME:
+    this.currentUserSubject.next({
+      id: 123,
+      username: 'gives_stuff99',
+      email: 'gives_stuff99@example.com',
+      picture: '/assets/test.png'
+    });
+  }
+}
