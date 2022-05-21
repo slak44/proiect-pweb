5f47053cec3ee8194affb3ad2823f06467040542
Stefan Silviu <stefan.silviu.alexandru@gmail.com>
2022-05-21 21:52:47 +0300

    Parse admin role from token

--- src/app/base/services/user.service.ts
+++ src/app/base/services/user.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, first, Observable, of } from 'rxjs';
+import { BehaviorSubject, distinctUntilChanged, first, map, Observable, of, shareReplay, switchMap } from 'rxjs';
 import { AppUser, UserType } from '../models/user.model';
 import { AuthService, User } from '@auth0/auth0-angular';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +13,20 @@ export class UserService {
 
   public readonly currentUser$: Observable<AppUser | null> = this.currentUserSubject;
 
-  public readonly isCurrentUserAdmin$: Observable<boolean> = of(false); // FIXME
+  public readonly isCurrentUserAdmin$: Observable<boolean> = this.currentUser$.pipe(
+    switchMap(() => this.authService.getAccessTokenSilently()),
+    map(token => {
+      const claimsBase64 = token.split('.')[1];
+      const claimsStr = atob(claimsBase64);
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const claims = JSON.parse(claimsStr);
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment
+      const roles: string[] = claims['https://pweb-app.example.com/roles'];
+
+      return roles.includes(UserType.ADMIN);
+    }),
+    shareReplay({ bufferSize: 1, refCount: false })
+  );
 
   constructor(
     private readonly authService: AuthService,
@@ -29,7 +43,7 @@ export class UserService {
         email: user.email!
       });
 
-      this.httpClient.get('http://localhost:8080/api/posts').subscribe(console.log);
+      // this.changeAccountType(user.sub!, UserType.ADMIN).subscribe(console.log);
     });
   }
 
@@ -38,40 +52,15 @@ export class UserService {
   }
 
   public getUserById(id: string): Observable<AppUser> {
-    // FIXME
-    return of({
-      id,
-      username: 'gives_stuff99',
-      email: 'gives_stuff99@example.com',
-      picture: '/assets/test.png',
-      type: UserType.USER,
-      isUserVerified: true,
-      isEnabled: true,
-    });
+    return this.httpClient.get<AppUser>(`${environment.baseUrl}/api/users/${id}`);
   }
 
   public getUserList(): Observable<AppUser[]> {
-    // FIXME
-    const a = {
-      id: '123546',
-      username: 'gives_stuff99',
-      email: 'gives_stuff99@example.com',
-      picture: '/assets/test.png',
-      type: UserType.USER,
-      isUserVerified: true,
-      isEnabled: true,
-    };
-
-    return of([
-      a, a, a, a, a,
-    ]);
+    return this.httpClient.get<AppUser[]>(`${environment.baseUrl}/api/users`);
   }
 
   public changeAccountType(userId: string, type: UserType): Observable<void> {
-    // FIXME
-    void userId;
-    void type;
-    return of(void null);
+    return this.httpClient.post<void>(`${environment.baseUrl}/api/users/${userId}/change-role/${type}`, {});
   }
 
   public updateEnabledState(userId: string, enabled: boolean): Observable<void> {
