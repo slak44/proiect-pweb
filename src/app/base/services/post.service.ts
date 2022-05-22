0411d52b3dd2e9516b6f3d892ec736d54ef0f267
Stefan Silviu <stefan.silviu.alexandru@gmail.com>
2022-05-22 16:36:48 +0300

    Add posts endpoints

--- src/app/base/services/post.service.ts
+++ src/app/base/services/post.service.ts
@@ -1,192 +1,63 @@
 import { Injectable } from '@angular/core';
-import { delay, Observable, of } from 'rxjs';
-import {
-  CreatePost,
-  CreateInteraction,
-  Post,
-  PostCategory,
-  PostType,
-  OwnedPost,
-  Interaction,
-} from '../models/post.model';
+import { Observable } from 'rxjs';
+import { CreateInteraction, CreatePost, OwnedPost, Post } from '../models/post.model';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostService {
-  constructor() {
+  constructor(
+    private readonly httpClient: HttpClient,
+  ) {
   }
 
   public getLatestPost(): Observable<Post> {
-    // FIXME
-    return of({
-      id: 123,
-      category: PostCategory.FOOD,
-      description: 'I am offering the thing in the picture. Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
-        'sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
-      imageUrl: '/assets/test-post.png',
-      isRetired: false,
-      ownerId: '123',
-      type: PostType.OFFER,
-      title: 'Offering free soup to people in this city',
-      upvotes: 54,
-      upvoted: true,
-      downvoted: false,
-      interactionCount: 2,
-      tags: [
-        'cool',
-        'Very Important And Stuff',
-        'puckfutin',
-        'hello',
-      ],
-    });
+    return this.httpClient.get<Post>(`${environment.baseUrl}/api/posts/latest`);
   }
 
   public getAllPosts(): Observable<Post[]> {
-    const a = (id: number) => ({
-      id,
-      category: PostCategory.FOOD,
-      description: 'I am offering the thing in the picture. Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
-        'sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
-      imageUrl: '/assets/test-post.png',
-      isRetired: false,
-      ownerId: '1221343',
-      type: PostType.OFFER,
-      title: 'Offering free soup to people in this city',
-      upvotes: 54,
-      upvoted: true,
-      downvoted: false,
-      interactionCount: 2,
-      tags: [
-        'cool',
-        'Very Important And Stuff',
-        'puckfutin',
-        'hello',
-      ],
-    });
-
-    return of([
-      a(Math.random() * 1000),
-      a(Math.random() * 1000),
-      a(Math.random() * 1000),
-      a(Math.random() * 1000),
-      a(Math.random() * 1000),
-      a(Math.random() * 1000),
-      a(Math.random() * 1000),
-      a(Math.random() * 1000),
-      a(Math.random() * 1000)
-    ]);
+    return this.httpClient.get<Post[]>(`${environment.baseUrl}/api/posts`);
   }
 
   public getMyPosts(): Observable<OwnedPost[]> {
-    const i: Interaction = {
-      phone: '0777777777',
-      text: 'I need it for something that is very very very very very very important',
-      email: 'gigel@example.com',
-      name: 'Gigel Popescu'
-    };
-
-    const a = (id: number) => ({
-      id,
-      category: PostCategory.FOOD,
-      description: 'I am offering the thing in the picture. Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
-        'sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
-      imageUrl: '/assets/test-post.png',
-      isRetired: false,
-      ownerId: '123',
-      type: PostType.OFFER,
-      title: 'Offering free soup to people in this city',
-      upvotes: 54,
-      upvoted: true,
-      downvoted: false,
-      interactionCount: 2,
-      tags: [
-        'cool',
-        'Very Important And Stuff',
-        'puckfutin',
-        'hello',
-      ],
-      interactions: [
-        i, i, i, i
-      ]
-    });
-
-    return of([
-      a(Math.random() * 1000),
-      a(Math.random() * 1000),
-      a(Math.random() * 1000),
-      a(Math.random() * 1000),
-      a(Math.random() * 1000),
-      a(Math.random() * 1000),
-      a(Math.random() * 1000),
-      a(Math.random() * 1000),
-      a(Math.random() * 1000)
-    ]);
+    return this.httpClient.get<OwnedPost[]>(`${environment.baseUrl}/api/posts/get-user-posts`);
   }
 
   public addTags(postId: number, tags: string[]): Observable<void> {
-    // FIXME
-    void postId;
-    void tags;
-    return of(void null);
+    return this.httpClient.post<void>(`${environment.baseUrl}/api/posts/${postId}/add-tags`, { tags });
   }
 
-  public removeTag(postId: number, tagText: string): Observable<void> {
-    // FIXME
-    void postId;
-    void tagText;
-    return of(void null);
+  public removeTag(postId: number, tag: string): Observable<void> {
+    return this.httpClient.post<void>(`${environment.baseUrl}/api/posts/${postId}/remove-tag`, { tag });
   }
 
   public upvote(postId: number): Observable<void> {
-    // FIXME
-    void postId;
-    return of(void null);
+    return this.httpClient.post<void>(`${environment.baseUrl}/api/posts/${postId}/upvote`, {});
   }
 
   public downvote(postId: number): Observable<void> {
-    // FIXME
-    void postId;
-    return of(void null);
+    return this.httpClient.post<void>(`${environment.baseUrl}/api/posts/${postId}/downvote`, {});
   }
 
   public createPost(createPost: CreatePost): Observable<Post> {
-    // FIXME
-    return of({
-      id: 12354,
-      ...createPost,
-      upvotes: 54,
-      upvoted: true,
-      downvoted: false,
-      interactionCount: 2,
-      isRetired: false,
-      ownerId: '123',
-    }).pipe(delay(2000));
+    return this.httpClient.post<Post>(`${environment.baseUrl}/api/posts`, createPost);
   }
 
   public interact(postId: number, interaction: CreateInteraction): Observable<void> {
-    // FIXME
-    void postId;
-    void interaction;
-    return of(void null);
+    return this.httpClient.post<void>(`${environment.baseUrl}/api/posts/${postId}/interact`, interaction);
   }
 
   public deletePost(postId: number): Observable<void> {
-    // FIXME
-    void postId;
-    return of(void null);
+    return this.httpClient.delete<void>(`${environment.baseUrl}/api/posts/${postId}`);
   }
 
   public retirePost(postId: number): Observable<void> {
-    // FIXME
-    void postId;
-    return of(void null);
+    return this.httpClient.post<void>(`${environment.baseUrl}/api/posts/${postId}/retire`, {});
   }
 
-  public editDescription(postId: number, newDescription: string): Observable<void> {
-    // FIXME
-    void postId;
-    void newDescription;
-    return of(void null);
+  public editDescription(postId: number, description: string): Observable<void> {
+    return this.httpClient.post<void>(`${environment.baseUrl}/api/posts/${postId}/edit-description`, { description });
   }
 }
